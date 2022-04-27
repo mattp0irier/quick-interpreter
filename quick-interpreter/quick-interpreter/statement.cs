3e3eff2c887ab2d692f233c33880d8389736416f
mattp0irier <poiriermatthewj@gmail.com>
2022-04-27 00:05:46 -0500

    state mint

--- quick-interpreter/quick-interpreter/statement.cs
+++ quick-interpreter/quick-interpreter/statement.cs
@@ -30,6 +30,9 @@ namespace quick_interpreter
     public class GenerateStmt : Statement
     {
         // characteristics?
+        public readonly Token testName;
+        public readonly int quantity;
+        public readonly bool shuffle;
 
         public override T accept<T>(Visitor<T> visitor)
         {
@@ -40,6 +43,8 @@ namespace quick_interpreter
     public class TestStmt : Statement
     {
         // characteristics?
+        public readonly Token name;
+        public readonly List<Question> questions;
 
         public override T accept<T>(Visitor<T> visitor)
         {
@@ -68,6 +73,10 @@ namespace quick_interpreter
     public class QuestionStmt : Statement
     {
         // characteristics?
+        public readonly Token bank;
+        public readonly Token type;
+        public readonly List<string> options;
+        public readonly int solution;
 
         public override T accept<T>(Visitor<T> visitor)
         {
@@ -94,6 +103,7 @@ namespace quick_interpreter
     public class ShuffleStmt : Statement
     {
         // characteristics?
+        // does this need any????
 
         public override T accept<T>(Visitor<T> visitor)
         {
@@ -104,6 +114,8 @@ namespace quick_interpreter
     public class DeleteStmt : Statement
     {
         // characteristics?
+        public readonly Token bank;
+        public readonly int index;
 
         public override T accept<T>(Visitor<T> visitor)
         {
@@ -114,6 +126,9 @@ namespace quick_interpreter
     public class SetStmt : Statement
     {
         // characteristics?
+        public readonly Token bank;
+        public readonly int index;
+        public readonly Token answer;
 
         public override T accept<T>(Visitor<T> visitor)
         {
